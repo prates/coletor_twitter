e76b54279040084c8a966d435faabdac39901ce3
Alexandre Prates <alexandre.b.prates@gmail.com>
2016-12-02 02:01:17 -0200

    Escrevendo o REadme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # coletor_twitter
+
+
+# Desenvovido por Alexandre Prates
+
+
+
+Descrição
+
+Essa aplicação utiliza a streamapi do twitter para coletar tweet com informações de geolocalização. Essa aplicação utiliza o REdis para persistir a informação, abaixo observamos 
+como instalar o redis.
+
+Exemplo de inicialização do Redis n máquina local:
+$redis-server
+
+Exemplo de uso de redis no docker
+
+# docker run --name redis -d -p 6379:6379 redis
+
+A implementação e testes foram feito utilizando Ubuntu linux.
+
+
+
+Procediemnto de instalação:
+
+1 - Crie um diretorio;
+2 - Utilize o virtualenv para criar um container python2.7
+$virtualenv diretorio
+3 - Entre no diretorio e copia a pasta do projeto;
+4 - Mude o source do bash para usar a versão do Python instalada no diretorio:
+$ source bin/active
+5 - Instale as dependencias do python, elas estão no arquivo requiment.txt do projeto. Para instala-las usei o pip:
+$pip install -r requeriment.txt
+6 - Apos instala r as dependencia execute o coletor da seguinte maneira:
+$ python geoteste.py "nome da cidade" raio em relaçãop ao centro.
+
--- geoteste.py
+++ geoteste.py
@@ -1,19 +1,14 @@
 import os
 import sys
 import math
-import time
-from datetime import datetime
 import json 
+import doctest
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 from geopy import geocoders
 import redis
 
-adress=str(sys.argv[1]) #Location (City name, adress...)
-halfradius=float(sys.argv[2]) #Radius in Km of the Bounding Box
-outputlocation=str(sys.argv[3])
-datereq = datetime.utcnow().strftime("%d%m%Y")
 
 #Change them with your authotenfication tokens
 ckey = 'B4qL5xWeoAtT4HDeM8fPRTUau'
@@ -23,7 +18,7 @@ asecret = 'yHE559wpPnHJmFzeFY9JOHSUO5zSQhxGhJnZcGNEOWArR'
 #################################
 
 
-class redisInterface():
+class RedisInterface():
 
     conn = None
 
@@ -36,7 +31,6 @@ class redisInterface():
 class listener(StreamListener):
     def on_data(self, data):
         data = json.loads(data)
-        logfile = open(os.path.join(outputlocation,adress+"-"+datereq+"-Request.csv"), "a")
         if data['coordinates']!=None:
             chaves = ['text', 'coordinates']
             data_formatada = {}
@@ -47,65 +41,80 @@ class listener(StreamListener):
                     else:
                         data_formatada[i] = data[i]
             print data_formatada
-            red = redisInterface()
+            red = RedisInterface()
             red.connect(host='localhost', port=6379, db=0)
             red.grafaDados(data_formatada)
             #print >>logfile, stream_log
             
         else:
             print "Sem geo"
-        logfile.close()
         return True
 
     def on_error(self, statut):
         print statut
-
-# degrees to radians
-def deg2rad(degrees):
-    return math.pi*degrees/180.0
-# radians to degrees
-def rad2deg(radians):
-    return 180.0*radians/math.pi
-# Semi-axes of WGS-84 geoidal reference
-WGS84_a = 6378137.0  # Major semiaxis [m]
-WGS84_b = 6356752.3  # Minor semiaxis [m]
-
-# Earth radius at a given latitude, according to the WGS-84 ellipsoid [m]
-def WGS84EarthRadius(lat):
-    # http://en.wikipedia.org/wiki/Earth_radius
-    An = WGS84_a*WGS84_a * math.cos(lat)
-    Bn = WGS84_b*WGS84_b * math.sin(lat)
-    Ad = WGS84_a * math.cos(lat)
-    Bd = WGS84_b * math.sin(lat)
-    return math.sqrt( (An*An + Bn*Bn)/(Ad*Ad + Bd*Bd) )
-
-# Bounding box surrounding the point at given coordinates,
-# assuming local approximation of Earth surface as a sphere
-# of radius given by WGS84
-def boundingBox(latitudeInDegrees, longitudeInDegrees, halfSideInKm):
-    lat = deg2rad(latitudeInDegrees)
-    lon = deg2rad(longitudeInDegrees)
-    halfSide = 1000*halfSideInKm
-
-    # Radius of Earth at given latitude
-    radius = WGS84EarthRadius(lat)
-    # Radius of the parallel at given latitude
-    pradius = radius*math.cos(lat)
-
-    latMin = lat - halfSide/radius
-    latMax = lat + halfSide/radius
-    lonMin = lon - halfSide/pradius
-    lonMax = lon + halfSide/pradius
-
-    return (rad2deg(latMin), rad2deg(lonMin), rad2deg(latMax), rad2deg(lonMax))
+        
+class TrataGeo():
+    
+    # Semi-axes of WGS-84 geoidal reference
+    __WGS84_a = 6378137.0  # Major semiaxis [m]
+    __WGS84_b = 6356752.3  # Minor semiaxis [m]
+    
+    
+    # degrees to radians
+    def deg2rad(self, degrees):
+        '''
+        >>> t = TrataGeo()
+        >>> rad = t.deg2rad(10)
+        >>> grad = t.rad2deg(rad)
+        10
+        '''
+        return math.pi*degrees/180.0
+    # radians to degrees
+    def rad2deg(self, radians):
+        return 180.0*radians/math.pi
+    
+    # Earth radius at a given latitude, according to the WGS-84 ellipsoid [m]
+    def WGS84EarthRadius(self, lat):
+        # http://en.wikipedia.org/wiki/Earth_radius
+        An = self.__WGS84_a * self.__WGS84_a * math.cos(lat)
+        Bn = self.__WGS84_b * self.__WGS84_b * math.sin(lat)
+        Ad = self.__WGS84_a * math.cos(lat)
+        Bd = self.__WGS84_b * math.sin(lat)
+        return math.sqrt( (An*An + Bn*Bn)/(Ad*Ad + Bd*Bd) )
+    
+    # Bounding box surrounding the point at given coordinates,
+    # assuming local approximation of Earth surface as a sphere
+    # of radius given by WGS84
+    def bounding_box(self, latitudeInDegrees, longitudeInDegrees, halfSideInKm):
+        lat = self.deg2rad(latitudeInDegrees)
+        lon = self.deg2rad(longitudeInDegrees)
+        halfSide = 1000*halfSideInKm
+    
+        # Radius of Earth at given latitude
+        radius = self.WGS84EarthRadius(lat)
+        # Radius of the parallel at given latitude
+        pradius = radius*math.cos(lat)
+    
+        latMin = lat - halfSide/radius
+        latMax = lat + halfSide/radius
+        lonMin = lon - halfSide/pradius
+        lonMax = lon + halfSide/pradius
+    
+        return (self.rad2deg(latMin), self.rad2deg(lonMin), self.rad2deg(latMax), self.rad2deg(lonMax))
 
 def main():
+    
+    adress=str(sys.argv[1]) #Location (City name, adress...)
+    halfradius=float(sys.argv[2]) #Radius in Km of the Bounding Box
     g = geocoders.GoogleV3()
     place, (lat, lng) = g.geocode(adress)
-    location = [boundingBox(lng,lat,halfradius)[0],boundingBox(lng,lat,halfradius)[1],boundingBox(lng,lat,halfradius)[2],boundingBox(lng,lat,halfradius)[3]]
+    geo = TrataGeo()
+    location = [geo.bounding_box(lng,lat,halfradius)[0],geo.bounding_box(lng,lat,halfradius)[1],geo.bounding_box(lng,lat,halfradius)[2],geo.bounding_box(lng,lat,halfradius)[3]]
     # print "Location of "+ adress+" :",lng,lat
     auth = OAuthHandler(ckey, csecret)
     auth.set_access_token(atoken, asecret)
     twitterStream = Stream(auth, listener())
     twitterStream.filter(locations=location)
+    
+   
 main()
